cd647285a1c56c0b764448b41942a7411ed0ca3d
Mark Benvenuto <mark.benvenuto@mongodb.com>
2020-02-25 19:02:07 -0500

    Always convert json logs

--- src/reducers/logProcessor/resmoke.js
+++ src/reducers/logProcessor/resmoke.js
@@ -20,9 +20,133 @@ function getFullGitRef(fileLine: ?string, gitVersion: string): ?string {
   return gitPrefix + gitVersion + '/' + fileLine;
 }
 
+function padEnd(str, len) {
+  if(str.length < len) {
+      return str + ' '.repeat(len - str.length );
+  }
+  return str;
+}
+class LogFormatter {
+
+  // var LOG_FORMAT_PREFIX = '{"t":{"$date';
+  // LOG_ERROR_REGEX = 'invariant|fassert|failed to load|uncaught exception';
+  // LOG_ATTR_REGEX = '\{([\w]+)\}';
+
+  constructor() {
+      this.reAttr = /{(\w+)\}/g;
+  }
+
+  format_line(
+      date,
+      log_level,
+      component,
+      context,
+      id,
+      msg
+  ) {
+      return `${date} ${padEnd(log_level, 2)} ${padEnd(component,8)} [${context}] ${msg}`;
+  }
+
+  log_to_str(str) {
+
+      let parsed = JSON.parse(str);
+
+      let d = parsed["t"]["$date"];
+      let log_level = parsed["s"]
+      let component = parsed["c"];
+      let log_id = parsed["id"];
+      let context = parsed["ctx"];
+      let msg = parsed["msg"];
+      let attr = parsed["attr"];
+
+      if (msg.indexOf("{") >= 0) {
+          // Handle messages which are just an empty {}
+          if (msg === "{}") {
+              return this.format_line(
+                  d,
+                  log_level,
+                  component,
+                  context,
+                  log_id,
+                  attr["message"]);
+          }
+
+
+          let msg_fmt = msg.replace(this.reAttr, function replacer(match, capture1, offset, str) {
+              
+              // TODO - objs?
+              // TODO - missing?
+              let ret =  attr[capture1];
+              if (typeof ret === 'object') {
+                  return JSON.stringify(ret);
+              }
+              return ret; 
+          });
+
+          return this.format_line(
+              d,
+              log_level,
+              component,
+              context,
+              log_id,
+              msg_fmt
+          );
+      } else {
+          if (attr !== undefined) {
+              let s1 = msg + attr;
+              return this.format_line(
+                  d,
+                  log_level,
+                  component,
+                  context,
+                  log_id,
+                  s1
+              );
+          }
+
+          return this.format_line(
+              d,
+              log_level,
+              component,
+              context,
+              log_id,
+              msg
+          );
+      }
+  }
+
+  fuzzy_log_to_str(str) {
+      const LOG_FORMAT_PREFIX = '{"t":{"$date';
+      if (str.startsWith(LOG_FORMAT_PREFIX)) {
+          return this.log_to_str(str);
+      }
+
+      // TODO - become stateful and rember where we found a previous start
+      let pos = str.indexOf(LOG_FORMAT_PREFIX);
+      if (pos !== -1) {
+          return str.substring(0, pos) + this.log_to_str(str.substring(pos));
+      }
+
+      // We do not think it is a JSON log line, return it as is
+      return str;
+  }
+}
+
+
 export default function(state: Log, response: string): Log {
   // set the url to the url we requested
-  const lines = response.split('\n');
+  let lines = response.split('\n');
+
+  let linestr = "";
+  const lf = new LogFormatter();
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    linestr += lf.fuzzy_log_to_str(line);
+    linestr += "\n";
+  }
+
+   lines = linestr.split('\n');
+
   const processed = [];
   const gitPrefix = '{githash:';
   const gitPrefixLen = gitPrefix.length + 2;
